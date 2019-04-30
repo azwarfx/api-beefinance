99d7585d59a9c713144c7b763864102514670763
azwarazuhri <data.azwarazuhri@gmail.com>
2019-04-30 13:10:05 +0700

    merubah struktur table dll

--- src/main/java/com/azwarazuhri/apibeefinance/entity/Users.java
+++ src/main/java/com/azwarazuhri/apibeefinance/entity/Users.java
@@ -15,6 +15,8 @@ public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	private String username;
-	private String password;
+	private String name;
+	private String email;
+	private String firstname;
+	private String lastname;
 }

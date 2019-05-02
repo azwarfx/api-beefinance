926e7e3ea1ca13bee965848ff74371526c985a4f
azwarazuhri <data.azwarazuhri@gmail.com>
2019-05-02 16:27:13 +0700

    upload 2 may 2019

--- src/main/java/com/azwarazuhri/apibeefinance/controller/LoginController.java
+++ src/main/java/com/azwarazuhri/apibeefinance/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.azwarazuhri.apibeefinance.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,11 +30,33 @@ public class LoginController {
 	  }
 
 
-	  @RequestMapping(value ="login", method = RequestMethod.POST)
-	 public @ResponseBody
-	  List<Users> login(String email){
+//	  @RequestMapping(value ="login", method = RequestMethod.POST)
+//	 public @ResponseBody
+//	  List<Users> login(String email){
+		  //create ObjectMapper instance
+//		  ObjectMapper objectMapper = new ObjectMapper();
+//		  Gson gson = new Gson();
+//		List<Users>usersList=userRepository.findByEmail(email);
+//		List<Users>usersListData=new ArrayList<>();
+//		for(Users users:usersList){
+//			Users usersData = new Users();
+//			usersData.setId(users.getId());
+//			usersData.setEmail(users.getEmail());
+//			usersData.setName(users.getName());
+//			usersData.setFirstname(users.getFirstname());
+//			usersData.setLastname(users.getLastname());
+//			usersListData.add(usersData);
+//			//gson.toJson(usersData);
+//		}
+//		return usersListData;
+//	  }
+
+	@RequestMapping(value ="login", method = RequestMethod.POST)
+	public @ResponseBody
+	  String test(String email){
+		Gson gson = new Gson();
+		String jsonInString=null;
 		List<Users>usersList=userRepository.findByEmail(email);
-		List<Users>usersListData=new ArrayList<>();
 		for(Users users:usersList){
 			Users usersData = new Users();
 			usersData.setId(users.getId());
@@ -38,8 +64,10 @@ public class LoginController {
 			usersData.setName(users.getName());
 			usersData.setFirstname(users.getFirstname());
 			usersData.setLastname(users.getLastname());
-			usersListData.add(usersData);
+			//Java object to JSON string
+			jsonInString= gson.toJson(usersData);
 		}
-		return usersListData;
+
+		return jsonInString;
 	  }
 }
